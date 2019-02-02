1ad609d7e6b3911865dffb5e862130e04947964f
iraborysenko <aurorasray@gmail.com>
2019-02-02 23:04:27 +0200

    added tip and portrait orientation

--- app/src/main/java/com/borysenko/listtobuy/ui/main/MainActivity.java
+++ app/src/main/java/com/borysenko/listtobuy/ui/main/MainActivity.java
@@ -1,12 +1,17 @@
 package com.borysenko.listtobuy.ui.main;
 
+import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.borysenko.listtobuy.R;
 import com.borysenko.listtobuy.ui.main.boughttab.BoughtFragment;
@@ -39,10 +44,41 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         if (id == R.id.action_settings) {
+            showTipMessage();
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressLint("SetTextI18n")
+    private void showTipMessage() {
+        TextView title = new TextView(this);
+        title.setText("Подсказка");
+        title.setPadding(10, 15, 10, 5);
+        title.setGravity(Gravity.CENTER);
+        title.setTextSize(22);
+
+        TextView message = new TextView(this);
+        message.setText("Long click на любой пункт списка 'Покупки' открывает/закрывает"
+                + " дополнительные кнопки\n(Перенос всего списка,\nПеренос выбранных"
+                + " пунктов,\nУдаление выбранных пунктов) ");
+        message.setPadding(10, 10, 10, 0);
+        message.setGravity(Gravity.CENTER);
+        message.setTextSize(18);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setCustomTitle(title)
+                .setView(message)
+                .setCancelable(false)
+                .setNegativeButton("ОК",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+        AlertDialog alert = builder.create();
+        alert.show();
     }
 
     public Fragment setupViewPager(ViewPager viewPager) {
